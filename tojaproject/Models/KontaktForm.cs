e28fbe2e3b18fcf599cdc55278aea6a26abda55b
HrAagesen <33120123+HrAagesen@users.noreply.github.com>
2020-02-17 14:05:38 +0100

    start

---
+++ tojaproject/Models/KontaktForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tojaproject.Models
+{
+    public class KontaktForm
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Telefonnr { get; set; }
+        public char Kommentar {get; set;}
+    }
+}
